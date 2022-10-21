711507510861f703267b7d43039365ab2808961e
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2022-10-21 17:13:00 +0200

    Added a collection for property editor content extractors

---
+++ src/Core/code/Indexing/Collections/PropertyEditorContentExtractorCollection.cs
@@ -0,0 +1,24 @@
+// <copyright file="PropertyEditorContentExtractorCollection.cs" company="Umbraco Community">
+// Copyright (c) Dave Woestenborghs
+// </copyright>
+
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Community.Indekso.Core.Indexing.Extractors;
+
+namespace Umbraco.Community.Indekso.Core.Indexing.Collections
+{
+    /// <summary>
+    /// Represents a collection of <see cref="IPropertyEditorContentExtractor"/> implementations.
+    /// </summary>
+    public sealed class PropertyEditorContentExtractorCollection : BuilderCollectionBase<IPropertyEditorContentExtractor>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyEditorContentExtractorCollection"/> class.
+        /// </summary>
+        /// <param name="items">A <see cref="Func{TResult}"/> that get's the implementations of <see cref="IPropertyEditorContentExtractor"/>.</param>
+        public PropertyEditorContentExtractorCollection(Func<IEnumerable<IPropertyEditorContentExtractor>> items)
+            : base(items)
+        {
+        }
+    }
+}
