1ec4b52d8eda7f649aaca7ed2312030f84929544
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2022-10-24 13:17:12 +0200

    Added event to listen to doctype saved event

--- src/BackOffice/code/wwwroot/app.controller.js
+++ src/BackOffice/code/wwwroot/app.controller.js
@@ -1,15 +1,28 @@
-(function() {
+(function () {
   'use strict';
 
-  function appController($scope) {
+  function appController($scope, eventsService) {
+
+    var unsubscribe = [];
 
     console.log($scope.model);
 
     var vm = this;
 
     console.log(vm);
+
+    unsubscribe.push(eventsService.on("editors.documentType.saved",
+      function () {
+        console.log('Doc type saved');
+
+      }));
+
+    vm.$onDestroy = function () {
+      console.log('on destroy called');
+      unsubscribe.forEach(x => x());
+    }
   }
 
   angular.module('umbraco').controller('Umbraco.Community.Indekso.AppController',
-    ['$scope', appController]);
+    ['$scope', 'eventsService', appController]);
 })();
