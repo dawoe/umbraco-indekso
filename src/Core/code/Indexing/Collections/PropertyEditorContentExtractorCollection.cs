32223b54ec9d6f57b0f24578b937a97bd6e2061c
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2022-10-24 10:44:45 +0200

    Added method to property editor content extractor to get registered extractor for a property type

--- src/Core/code/Indexing/Collections/PropertyEditorContentExtractorCollection.cs
+++ src/Core/code/Indexing/Collections/PropertyEditorContentExtractorCollection.cs
@@ -3,6 +3,7 @@
 // </copyright>
 
 using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Community.Indekso.Core.Indexing.Extractors;
 
 namespace Umbraco.Community.Indekso.Core.Indexing.Collections
@@ -12,6 +13,9 @@ namespace Umbraco.Community.Indekso.Core.Indexing.Collections
     /// </summary>
     public sealed class PropertyEditorContentExtractorCollection : BuilderCollectionBase<IPropertyEditorContentExtractor>
     {
+        private readonly object locker = new();
+        private Dictionary<IPublishedPropertyType, IList<IPropertyEditorContentExtractor>>? extractors;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="PropertyEditorContentExtractorCollection"/> class.
         /// </summary>
@@ -19,6 +23,30 @@ namespace Umbraco.Community.Indekso.Core.Indexing.Collections
         public PropertyEditorContentExtractorCollection(Func<IEnumerable<IPropertyEditorContentExtractor>> items)
             : base(items)
         {
+        }
+
+        /// <summary>
+        /// Gets the registered extractors for a property type.
+        /// </summary>
+        /// <param name="propertyType">A <see cref="IPublishedPropertyType"/> to get the extractors for.</param>
+        /// <returns>A <see cref="IList{T}"/> of registered extractors for the property type.</returns>
+        public IList<IPropertyEditorContentExtractor> GetExtractorsForPropertyType(IPublishedPropertyType propertyType)
+        {
+            lock (this.locker)
+            {
+                this.extractors ??= new Dictionary<IPublishedPropertyType, IList<IPropertyEditorContentExtractor>>();
+
+                if (this.extractors.ContainsKey(propertyType))
+                {
+                    return this.extractors[propertyType];
+                }
+
+                var foundExtractors = this.Where(x => x.IsExtractFor(propertyType)).ToList();
+
+                this.extractors.Add(propertyType, foundExtractors);
+
+                return foundExtractors;
+            }
         }
     }
 }
